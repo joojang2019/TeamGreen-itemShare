3265aa936fe58d35e61502f52dc036abdeb32fdd
Aakanksha Ardhapurkar <aakanksha720@gmail.com>
2020-01-13 10:37:09 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -8,50 +8,50 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 
-const data = {
-  "items": [
-    {
-      "name": "Canon 555",
-      "type": "camera",
-      "location": "Foster-Walker Complex",
-      "img": "https://static.bhphoto.com/images/images500x500/1566949680_1502489.jpg",
-      "user": "joo123",
-      "availableTill": "June 11th 2020",
-      "price": "3.99",
-      "email": "joo@young.com"
-    },
-    {
-      "name": "Cool Bike 255",
-      "type": "bike",
-      "location": "Elder",
-      "img": "https://images.giant-bicycles.com/b_white,c_pad,h_650,q_80/orcopm04xg7xrdmhpwo8/MY19TranceSXE+0Pro_ColorA.jpg",
-      "user": "andrew22",
-      "availableTill": "June 1st 2020",
-      "price": "5.50",
-      "email": "andrew@chung.com"
-    },
-    {
-      "name": "Used bike 333",
-      "type": "bike",
-      "location": "1500 Chicago Avenue",
-      "img": "https://vader-prod.s3.amazonaws.com/1557768743-state-the-keansburg-bike-1557768701.jpg",
-      "user": "bambam",
-      "availableTill": "June 13th 2020",
-      "price": "20.99",
-      "email": "bambam@gmail.com"
-    },
-    {
-      "name": "sick camera 555",
-      "type": "camera",
-      "location": "Foster-Walker Complex",
-      "img": "https://icdn6.digitaltrends.com/image/digitaltrends/fujifilm-x-t30-hands-on-7174-2-720x720.jpg",
-      "user": "batubatu",
-      "availableTill": "May 10th 2020",
-      "price": "3.99",
-      "email": "batu@batu.com"
-    }
-  ]
-};
+// const data = {
+//   "items": [
+//     {
+//       "name": "Canon 555",
+//       "type": "camera",
+//       "location": "Foster-Walker Complex",
+//       "img": "https://static.bhphoto.com/images/images500x500/1566949680_1502489.jpg",
+//       "user": "joo123",
+//       "availableTill": "June 11th 2020",
+//       "price": "3.99",
+//       "email": "joo@young.com"
+//     },
+//     {
+//       "name": "Cool Bike 255",
+//       "type": "bike",
+//       "location": "Elder",
+//       "img": "https://images.giant-bicycles.com/b_white,c_pad,h_650,q_80/orcopm04xg7xrdmhpwo8/MY19TranceSXE+0Pro_ColorA.jpg",
+//       "user": "andrew22",
+//       "availableTill": "June 1st 2020",
+//       "price": "5.50",
+//       "email": "andrew@chung.com"
+//     },
+//     {
+//       "name": "Used bike 333",
+//       "type": "bike",
+//       "location": "1500 Chicago Avenue",
+//       "img": "https://vader-prod.s3.amazonaws.com/1557768743-state-the-keansburg-bike-1557768701.jpg",
+//       "user": "bambam",
+//       "availableTill": "June 13th 2020",
+//       "price": "20.99",
+//       "email": "bambam@gmail.com"
+//     },
+//     {
+//       "name": "sick camera 555",
+//       "type": "camera",
+//       "location": "Foster-Walker Complex",
+//       "img": "https://icdn6.digitaltrends.com/image/digitaltrends/fujifilm-x-t30-hands-on-7174-2-720x720.jpg",
+//       "user": "batubatu",
+//       "availableTill": "May 10th 2020",
+//       "price": "3.99",
+//       "email": "batu@batu.com"
+//     }
+//   ]
+// };
 
 
 // Firebase Config
@@ -65,7 +65,7 @@ const firebaseConfig = {
     appId: "1:512831900868:web:c3e2d30285e6584d0fcd91",
     measurementId: "G-NHVDY8ZB8W"
 };
-
+   
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database().ref();
 
@@ -76,7 +76,7 @@ const App = () =>  {
     <div>
       <Banner />
       <Search state={{searchTerm, setSearchTerm}} />
-      <ItemList items={data.items}/>
+      {/* <ItemList items={data.items}/> */}
   </div>
   );
   
