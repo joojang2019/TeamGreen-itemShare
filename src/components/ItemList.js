2b42694c880aec18ade18125bc58b66730e0e23f
Batuhan Ergor <batuhanergor2021@u.northwestern.edu>
2020-01-30 12:14:42 -0600

    result not found

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Item from "./Item";
+import SearchIcon from "@material-ui/icons/Search";
 
 const ItemList = ({ items }) => {
   return items.length ? (
     items.map(([id, item]) => <Item key={id} item={item} />)
   ) : (
-    <div style={{ textAlign: "center" }}>No results found.</div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateRows: "1fr 1fr",
+        textAlign: "center",
+        padding: 25
+      }}
+    >
+      <div>
+        <SearchIcon style={{ fontSize: "200", color: "coral" }}></SearchIcon>
+      </div>
+      <div>
+        We couldn't find the item you're looking for. Please try searching a
+        different item!
+      </div>
+    </div>
   );
 };
 
